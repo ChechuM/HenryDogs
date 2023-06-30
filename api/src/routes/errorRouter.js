359d35bbcc80a461d07bc32ad2a190fc0d4e2df3
ChechuM <ceciliammoroni@gmail.com>
2023-07-01 07:57:28 +1000

    por presentarr

--- api/src/routes/errorRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const errorRouter = Router();
-// const img = require('./412.jpg')
-
-errorRouter.get('/', (req, res) => {
-    // let errorImg = img;
-    res.status(200).json('This is an error')
-})
